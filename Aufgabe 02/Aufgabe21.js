b8789d6c7203e5c319a75a536264f1a699ac8f9b
Anja <Anja@Cata-Laptop>
2018-10-27 15:09:28 +0200

    grün

---
+++ Aufgabe 02/Aufgabe21.js
@@ -0,0 +1,307 @@
+var Aufgabe2besser;
+(function (Aufgabe2besser) {
+    /* Rote Karten */
+    let r1 = {
+        color: "red",
+        value: "0",
+    };
+    let r2 = {
+        color: "red",
+        value: "1",
+    };
+    let r3 = {
+        color: "red",
+        value: "2",
+    };
+    let r4 = {
+        color: "red",
+        value: "3",
+    };
+    let r5 = {
+        color: "red",
+        value: "4",
+    };
+    let r6 = {
+        color: "red",
+        value: "5",
+    };
+    let r7 = {
+        color: "red",
+        value: "6",
+    };
+    let r8 = {
+        color: "red",
+        value: "7",
+    };
+    let r9 = {
+        color: "red",
+        value: "8",
+    };
+    let r10 = {
+        color: "red",
+        value: "9",
+    };
+    let r11 = {
+        color: "red",
+        value: "take2"
+    };
+    let r12 = {
+        color: "red",
+        value: "reverse",
+    };
+    let r13 = {
+        color: "red",
+        value: "missturn",
+    };
+    let r14 = {
+        color: "red",
+        value: "1",
+    };
+    let r15 = {
+        color: "red",
+        value: "2",
+    };
+    let r16 = {
+        color: "red",
+        value: "3",
+    };
+    let r17 = {
+        color: "red",
+        value: "4",
+    };
+    let r18 = {
+        color: "red",
+        value: "5",
+    };
+    let r19 = {
+        color: "red",
+        value: "6",
+    };
+    let r20 = {
+        color: "red",
+        value: "7",
+    };
+    let r21 = {
+        color: "red",
+        value: "8",
+    };
+    let r22 = {
+        color: "red",
+        value: "9",
+    };
+    let r23 = {
+        color: "red",
+        value: "take2"
+    };
+    let r24 = {
+        color: "red",
+        value: "reverse",
+    };
+    let r25 = {
+        color: "red",
+        value: "missturn",
+    };
+    /* Gr�ne Karten */
+    let g1 = {
+        color: "blue",
+        value: "0",
+    };
+    let g2 = {
+        color: "blue",
+        value: "1",
+    };
+    let g3 = {
+        color: "blue",
+        value: "2",
+    };
+    let g4 = {
+        color: "blue",
+        value: "3",
+    };
+    let g5 = {
+        color: "blue",
+        value: "4",
+    };
+    let g6 = {
+        color: "blue",
+        value: "5",
+    };
+    let g7 = {
+        color: "blue",
+        value: "6",
+    };
+    let g8 = {
+        color: "blue",
+        value: "7",
+    };
+    let g9 = {
+        color: "blue",
+        value: "8",
+    };
+    let g10 = {
+        color: "blue",
+        value: "9",
+    };
+    let g11 = {
+        color: "blue",
+        value: "take2"
+    };
+    let g12 = {
+        color: "blue",
+        value: "reverse",
+    };
+    let g13 = {
+        color: "blue",
+        value: "missturn",
+    };
+    let g14 = {
+        color: "blue",
+        value: "1",
+    };
+    let g15 = {
+        color: "blue",
+        value: "2",
+    };
+    let g16 = {
+        color: "blue",
+        value: "3",
+    };
+    let g17 = {
+        color: "blue",
+        value: "4",
+    };
+    let g18 = {
+        color: "blue",
+        value: "5",
+    };
+    let g19 = {
+        color: "blue",
+        value: "6",
+    };
+    let g20 = {
+        color: "blue",
+        value: "7",
+    };
+    let g21 = {
+        color: "blue",
+        value: "8",
+    };
+    let g22 = {
+        color: "blue",
+        value: "9",
+    };
+    let g23 = {
+        color: "blue",
+        value: "take2"
+    };
+    let g24 = {
+        color: "blue",
+        value: "reverse",
+    };
+    let g25 = {
+        color: "blue",
+        value: "missturn",
+    };
+    /* Blaue Karten */
+    let b1 = {
+        color: "blue",
+        value: "0",
+    };
+    let b2 = {
+        color: "blue",
+        value: "1",
+    };
+    let b3 = {
+        color: "blue",
+        value: "2",
+    };
+    let b4 = {
+        color: "blue",
+        value: "3",
+    };
+    let b5 = {
+        color: "blue",
+        value: "4",
+    };
+    let b6 = {
+        color: "blue",
+        value: "5",
+    };
+    let b7 = {
+        color: "blue",
+        value: "6",
+    };
+    let b8 = {
+        color: "blue",
+        value: "7",
+    };
+    let b9 = {
+        color: "blue",
+        value: "8",
+    };
+    let b10 = {
+        color: "blue",
+        value: "9",
+    };
+    let b11 = {
+        color: "blue",
+        value: "take2"
+    };
+    let b12 = {
+        color: "blue",
+        value: "reverse",
+    };
+    let b13 = {
+        color: "blue",
+        value: "missturn",
+    };
+    let b14 = {
+        color: "blue",
+        value: "1",
+    };
+    let b15 = {
+        color: "blue",
+        value: "2",
+    };
+    let b16 = {
+        color: "blue",
+        value: "3",
+    };
+    let b17 = {
+        color: "blue",
+        value: "4",
+    };
+    let b18 = {
+        color: "blue",
+        value: "5",
+    };
+    let b19 = {
+        color: "blue",
+        value: "6",
+    };
+    let b20 = {
+        color: "blue",
+        value: "7",
+    };
+    let b21 = {
+        color: "blue",
+        value: "8",
+    };
+    let b22 = {
+        color: "blue",
+        value: "9",
+    };
+    let b23 = {
+        color: "blue",
+        value: "take2"
+    };
+    let b24 = {
+        color: "blue",
+        value: "reverse",
+    };
+    let b25 = {
+        color: "blue",
+        value: "missturn",
+    };
+})(Aufgabe2besser || (Aufgabe2besser = {}));
+//# sourceMappingURL=Aufgabe21.js.map
---
+++ Aufgabe 02/Aufgabe21.ts
@@ -0,0 +1,399 @@
+namespace Aufgabe2besser {
+/*
+Aufgabe: Aufgabe 2
+Name: Catherina Arndt
+Matrikel: 259418
+Datum: 20.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
+Er wurde nicht kopiert und auch nicht diktiert.
+*/
+        
+interface Cards {
+    color: string
+    value: string
+}
+  
+/* Rote Karten */
+    
+let r1: Cards = {
+    color: "red",
+    value: "0",
+}
+    
+let r2: Cards = {
+    color: "red",
+    value: "1",
+}
+
+let r3: Cards = {
+    color: "red",
+    value: "2",
+}
+    
+let r4: Cards = {
+    color: "red",
+    value: "3",
+}
+    
+let r5: Cards = {
+    color: "red",
+    value: "4",
+}
+
+let r6: Cards = {
+    color: "red",
+    value: "5",
+}
+
+let r7: Cards = {
+    color: "red",
+    value: "6",
+}
+    
+let r8: Cards = {
+    color: "red",
+    value: "7",
+}
+
+let r9: Cards = {
+    color: "red",
+    value: "8",
+}
+    
+let r10: Cards = {
+    color: "red", 
+    value: "9",
+}
+    
+let r11: Cards = {
+    color: "red",
+    value: "take2"
+}
+
+let r12: Cards = {
+    color: "red",
+    value: "reverse",
+}    
+   
+let r13: Cards = {
+    color: "red",
+    value: "missturn",
+} 
+    
+let r14: Cards = {
+    color: "red",
+    value: "1",
+}
+
+let r15: Cards = {
+    color: "red",
+    value: "2",
+}
+    
+let r16: Cards = {
+    color: "red",
+    value: "3",
+}
+    
+let r17: Cards = {
+    color: "red",
+    value: "4",
+}
+
+let r18: Cards = {
+    color: "red",
+    value: "5",
+}
+
+let r19: Cards = {
+    color: "red",
+    value: "6",
+}
+    
+let r20: Cards = {
+    color: "red",
+    value: "7",
+}
+
+let r21: Cards = {
+    color: "red",
+    value: "8",
+}
+    
+let r22: Cards = {
+    color: "red", 
+    value: "9",
+}
+    
+let r23: Cards = {
+    color: "red",
+    value: "take2"
+}
+
+let r24: Cards = {
+    color: "red",
+    value: "reverse",
+}    
+   
+let r25: Cards = {
+    color: "red",
+    value: "missturn",
+} 
+  
+    
+/* Grüne Karten */ 
+    
+let g1: Cards = {
+    color: "blue",
+    value: "0",
+}
+    
+let g2: Cards = {
+    color: "blue",
+    value: "1",
+}
+
+let g3: Cards = {
+    color: "blue",
+    value: "2",
+}
+    
+let g4: Cards = {
+    color: "blue",
+    value: "3",
+}
+    
+let g5: Cards = {
+    color: "blue",
+    value: "4",
+}
+
+let g6: Cards = {
+    color: "blue",
+    value: "5",
+}
+
+let g7: Cards = {
+    color: "blue",
+    value: "6",
+}
+    
+let g8: Cards = {
+    color: "blue",
+    value: "7",
+}
+
+let g9: Cards = {
+    color: "blue",
+    value: "8",
+}
+    
+let g10: Cards = {
+    color: "blue", 
+    value: "9",
+}
+    
+let g11: Cards = {
+    color: "blue",
+    value: "take2"
+}
+
+let g12: Cards = {
+    color: "blue",
+    value: "reverse",
+}    
+   
+let g13: Cards = {
+    color: "blue",
+    value: "missturn",
+} 
+    
+let g14: Cards = {
+    color: "blue",
+    value: "1",
+}
+
+let g15: Cards = {
+    color: "blue",
+    value: "2",
+}
+    
+let g16: Cards = {
+    color: "blue",
+    value: "3",
+}
+    
+let g17: Cards = {
+    color: "blue",
+    value: "4",
+}
+
+let g18: Cards = {
+    color: "blue",
+    value: "5",
+}
+
+let g19: Cards = {
+    color: "blue",
+    value: "6",
+}
+    
+let g20: Cards = {
+    color: "blue",
+    value: "7",
+}
+
+let g21: Cards = {
+    color: "blue",
+    value: "8",
+}
+    
+let g22: Cards = {
+    color: "blue", 
+    value: "9",
+}
+    
+let g23: Cards = {
+    color: "blue",
+    value: "take2"
+}
+
+let g24: Cards = {
+    color: "blue",
+    value: "reverse",
+}    
+   
+let g25: Cards = {
+    color: "blue",
+    value: "missturn",
+} 
+    
+/* Blaue Karten */
+    
+    
+let b1: Cards = {
+    color: "blue",
+    value: "0",
+}
+    
+let b2: Cards = {
+    color: "blue",
+    value: "1",
+}
+
+let b3: Cards = {
+    color: "blue",
+    value: "2",
+}
+    
+let b4: Cards = {
+    color: "blue",
+    value: "3",
+}
+    
+let b5: Cards = {
+    color: "blue",
+    value: "4",
+}
+
+let b6: Cards = {
+    color: "blue",
+    value: "5",
+}
+
+let b7: Cards = {
+    color: "blue",
+    value: "6",
+}
+    
+let b8: Cards = {
+    color: "blue",
+    value: "7",
+}
+
+let b9: Cards = {
+    color: "blue",
+    value: "8",
+}
+    
+let b10: Cards = {
+    color: "blue", 
+    value: "9",
+}
+    
+let b11: Cards = {
+    color: "blue",
+    value: "take2"
+}
+
+let b12: Cards = {
+    color: "blue",
+    value: "reverse",
+}    
+   
+let b13: Cards = {
+    color: "blue",
+    value: "missturn",
+} 
+    
+let b14: Cards = {
+    color: "blue",
+    value: "1",
+}
+
+let b15: Cards = {
+    color: "blue",
+    value: "2",
+}
+    
+let b16: Cards = {
+    color: "blue",
+    value: "3",
+}
+    
+let b17: Cards = {
+    color: "blue",
+    value: "4",
+}
+
+let b18: Cards = {
+    color: "blue",
+    value: "5",
+}
+
+let b19: Cards = {
+    color: "blue",
+    value: "6",
+}
+    
+let b20: Cards = {
+    color: "blue",
+    value: "7",
+}
+
+let b21: Cards = {
+    color: "blue",
+    value: "8",
+}
+    
+let b22: Cards = {
+    color: "blue", 
+    value: "9",
+}
+    
+let b23: Cards = {
+    color: "blue",
+    value: "take2"
+}
+
+let b24: Cards = {
+    color: "blue",
+    value: "reverse",
+}    
+   
+let b25: Cards = {
+    color: "blue",
+    value: "missturn",
+} 
+    
+}
