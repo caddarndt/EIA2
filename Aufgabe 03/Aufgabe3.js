e0cd0d95abe56b4656e7de4dd1abfae10d7a7f8a
Anja <Anja@Cata-Laptop.eduroam.hs-furtwangen.de>
2018-11-08 11:36:39 +0100

    dffhsdgsdf

---
+++ Aufgabe 03/Aufgabe3.js
@@ -0,0 +1,485 @@
+var Aufgabe3;
+(function (Aufgabe3) {
+    /* Rote Karten */
+    let r1 = {
+        color: "red",
+        value: "0",
+    };
+    let r2 = {
+        color: "red",
+        value: "1",
+    };
+    let r3 = {
+        color: "red",
+        value: "2",
+    };
+    let r4 = {
+        color: "red",
+        value: "3",
+    };
+    let r5 = {
+        color: "red",
+        value: "4",
+    };
+    let r6 = {
+        color: "red",
+        value: "5",
+    };
+    let r7 = {
+        color: "red",
+        value: "6",
+    };
+    let r8 = {
+        color: "red",
+        value: "7",
+    };
+    let r9 = {
+        color: "red",
+        value: "8",
+    };
+    let r10 = {
+        color: "red",
+        value: "9",
+    };
+    let r11 = {
+        color: "red",
+        value: "take2"
+    };
+    let r12 = {
+        color: "red",
+        value: "reverse",
+    };
+    let r13 = {
+        color: "red",
+        value: "missturn",
+    };
+    let r14 = {
+        color: "red",
+        value: "1",
+    };
+    let r15 = {
+        color: "red",
+        value: "2",
+    };
+    let r16 = {
+        color: "red",
+        value: "3",
+    };
+    let r17 = {
+        color: "red",
+        value: "4",
+    };
+    let r18 = {
+        color: "red",
+        value: "5",
+    };
+    let r19 = {
+        color: "red",
+        value: "6",
+    };
+    let r20 = {
+        color: "red",
+        value: "7",
+    };
+    let r21 = {
+        color: "red",
+        value: "8",
+    };
+    let r22 = {
+        color: "red",
+        value: "9",
+    };
+    let r23 = {
+        color: "red",
+        value: "take2"
+    };
+    let r24 = {
+        color: "red",
+        value: "reverse",
+    };
+    let r25 = {
+        color: "red",
+        value: "missturn",
+    };
+    /* Gr�ne Karten */
+    let g1 = {
+        color: "green",
+        value: "0",
+    };
+    let g2 = {
+        color: "green",
+        value: "1",
+    };
+    let g3 = {
+        color: "green",
+        value: "2",
+    };
+    let g4 = {
+        color: "green",
+        value: "3",
+    };
+    let g5 = {
+        color: "green",
+        value: "4",
+    };
+    let g6 = {
+        color: "green",
+        value: "5",
+    };
+    let g7 = {
+        color: "green",
+        value: "6",
+    };
+    let g8 = {
+        color: "green",
+        value: "7",
+    };
+    let g9 = {
+        color: "green",
+        value: "8",
+    };
+    let g10 = {
+        color: "green",
+        value: "9",
+    };
+    let g11 = {
+        color: "green",
+        value: "take2"
+    };
+    let g12 = {
+        color: "green",
+        value: "reverse",
+    };
+    let g13 = {
+        color: "green",
+        value: "missturn",
+    };
+    let g14 = {
+        color: "green",
+        value: "1",
+    };
+    let g15 = {
+        color: "green",
+        value: "2",
+    };
+    let g16 = {
+        color: "green",
+        value: "3",
+    };
+    let g17 = {
+        color: "green",
+        value: "4",
+    };
+    let g18 = {
+        color: "green",
+        value: "5",
+    };
+    let g19 = {
+        color: "green",
+        value: "6",
+    };
+    let g20 = {
+        color: "green",
+        value: "7",
+    };
+    let g21 = {
+        color: "green",
+        value: "8",
+    };
+    let g22 = {
+        color: "green",
+        value: "9",
+    };
+    let g23 = {
+        color: "green",
+        value: "take2"
+    };
+    let g24 = {
+        color: "green",
+        value: "reverse",
+    };
+    let g25 = {
+        color: "green",
+        value: "missturn",
+    };
+    /* Blaue Karten */
+    let b1 = {
+        color: "blue",
+        value: "0",
+    };
+    let b2 = {
+        color: "blue",
+        value: "1",
+    };
+    let b3 = {
+        color: "blue",
+        value: "2",
+    };
+    let b4 = {
+        color: "blue",
+        value: "3",
+    };
+    let b5 = {
+        color: "blue",
+        value: "4",
+    };
+    let b6 = {
+        color: "blue",
+        value: "5",
+    };
+    let b7 = {
+        color: "blue",
+        value: "6",
+    };
+    let b8 = {
+        color: "blue",
+        value: "7",
+    };
+    let b9 = {
+        color: "blue",
+        value: "8",
+    };
+    let b10 = {
+        color: "blue",
+        value: "9",
+    };
+    let b11 = {
+        color: "blue",
+        value: "take2"
+    };
+    let b12 = {
+        color: "blue",
+        value: "reverse",
+    };
+    let b13 = {
+        color: "blue",
+        value: "missturn",
+    };
+    let b14 = {
+        color: "blue",
+        value: "1",
+    };
+    let b15 = {
+        color: "blue",
+        value: "2",
+    };
+    let b16 = {
+        color: "blue",
+        value: "3",
+    };
+    let b17 = {
+        color: "blue",
+        value: "4",
+    };
+    let b18 = {
+        color: "blue",
+        value: "5",
+    };
+    let b19 = {
+        color: "blue",
+        value: "6",
+    };
+    let b20 = {
+        color: "blue",
+        value: "7",
+    };
+    let b21 = {
+        color: "blue",
+        value: "8",
+    };
+    let b22 = {
+        color: "blue",
+        value: "9",
+    };
+    let b23 = {
+        color: "blue",
+        value: "take2"
+    };
+    let b24 = {
+        color: "blue",
+        value: "reverse",
+    };
+    let b25 = {
+        color: "blue",
+        value: "missturn",
+    };
+    /* Gelbe Karten */
+    let y1 = {
+        color: "yellow",
+        value: "0",
+    };
+    let y2 = {
+        color: "yellow",
+        value: "1",
+    };
+    let y3 = {
+        color: "yellow",
+        value: "2",
+    };
+    let y4 = {
+        color: "yellow",
+        value: "3",
+    };
+    let y5 = {
+        color: "yellow",
+        value: "4",
+    };
+    let y6 = {
+        color: "yellow",
+        value: "5",
+    };
+    let y7 = {
+        color: "yellow",
+        value: "6",
+    };
+    let y8 = {
+        color: "yellow",
+        value: "7",
+    };
+    let y9 = {
+        color: "yellow",
+        value: "8",
+    };
+    let y10 = {
+        color: "yellow",
+        value: "9",
+    };
+    let y11 = {
+        color: "yellow",
+        value: "take2"
+    };
+    let y12 = {
+        color: "yellow",
+        value: "reverse",
+    };
+    let y13 = {
+        color: "yellow",
+        value: "missturn",
+    };
+    let y14 = {
+        color: "yellow",
+        value: "1",
+    };
+    let y15 = {
+        color: "yellow",
+        value: "2",
+    };
+    let y16 = {
+        color: "yellow",
+        value: "3",
+    };
+    let y17 = {
+        color: "yellow",
+        value: "4",
+    };
+    let y18 = {
+        color: "yellow",
+        value: "5",
+    };
+    let y19 = {
+        color: "yellow",
+        value: "6",
+    };
+    let y20 = {
+        color: "yellow",
+        value: "7",
+    };
+    let y21 = {
+        color: "yellow",
+        value: "8",
+    };
+    let y22 = {
+        color: "yellow",
+        value: "9",
+    };
+    let y23 = {
+        color: "yellow",
+        value: "take2"
+    };
+    let y24 = {
+        color: "yellow",
+        value: "reverse",
+    };
+    let y25 = {
+        color: "yellow",
+        value: "missturn",
+    };
+    /* Schwarze Karten */
+    let bb1 = {
+        color: "black",
+        value: "Farbwahl",
+    };
+    let bb2 = {
+        color: "black",
+        value: "Farbwahl",
+    };
+    let bb3 = {
+        color: "black",
+        value: "Farbwahl",
+    };
+    let bb4 = {
+        color: "black",
+        value: "Farbwahl",
+    };
+    let bb5 = {
+        color: "black",
+        value: "Farbwahl +4",
+    };
+    let bb6 = {
+        color: "black",
+        value: "Farbwahl +4",
+    };
+    let bb7 = {
+        color: "black",
+        value: "Farbwahl +4",
+    };
+    let bb8 = {
+        color: "black",
+        value: "Farbwahl +4",
+    };
+    /* Kartendeck */
+    let deck = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25,
+        b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25,
+        g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20, g21, g22, g23, g24, g25,
+        y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11, y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22, y23, y24, y25,
+        bb1, bb2, bb3, bb4, bb5, bb6, bb7, bb8];
+    /* Hauptfunktion */
+    function main() {
+        let cards;
+        let p = prompt("Mit wie vielen Karten willst du spielen?");
+        cards = Number(p);
+        if (cards > 10) {
+            cards = 10;
+        }
+        for (let i = 0; i < cards; i++) {
+            let randomCard = random(deck.length);
+            placeDiv(deck[randomCard].color, deck[randomCard].value, i);
+            deck.splice(randomCard, 1);
+            continue;
+        }
+    }
+    function random(_math) {
+        return Math.floor(Math.random() * Math.floor(_math));
+    }
+    function placeDiv(_color, _x, _n) {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "a" + _n);
+        document.getElementById("a" + _n).innerHTML += _x;
+        let s = div.style;
+        s.backgroundColor = _color;
+        s.border = "thin solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.width = 90 + "px";
+        s.height = 140 + "px";
+        s.right = 50 + "px";
+        s.top = 20 + "px";
+        s.borderRadius = 5 + "px";
+        s.color = "white";
+        s.left = (_n + 1) * 100 + "px";
+        s.right = (_n + 1) * 100 + "px";
+    }
+    document.addEventListener("DOMContentLoaded", main);
+})(Aufgabe3 || (Aufgabe3 = {}));
+//# sourceMappingURL=Aufgabe3.js.map
