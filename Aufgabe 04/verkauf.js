3e1277f1b675dad18f9d9ec4f596117653414e65
Anja <Anja@Cata-Laptop>
2018-11-18 13:50:04 +0100

    vb

--- Aufgabe 04/verkauf.js
+++ Aufgabe 04/verkauf.js
@@ -4,7 +4,7 @@ var Aufgabe4;
     Aufgabe4.ornament = [{ name: "Kugeln w�hlen,", price: 0 }, { name: "gold,", price: 3 }, { name: "silber,", price: 3 }, { name: "rot,", price: 5 }, { name: "blau,", price: 5 }, { name: "violett,", price: 5 }];
     Aufgabe4.lametta = [{ name: "Lametta w�hlen,", price: 0 }, { name: "gold,", price: 2.99 }, { name: "silber,", price: 2.99 }, { name: "rot,", price: 2.99 }, { name: "blau,", price: 2.99 }, { name: "violett,", price: 2.99 }];
     Aufgabe4.candles = [{ name: "Kerzen w�hlen,", price: 0 }, { name: "wei�,", price: 5 }, { name: "rot,", price: 5 }, { name: "gold,", price: 6 }, { name: "silber,", price: 6 }];
-    Aufgabe4.treestands = [{ name: "Baumst�nder w�hlen", price: 0 }, { name: "keinen,", price: 0 }, { name: "Holz rustikal,", price: 15 }, { name: "Metall silber,", price: 10 }, { name: "Keramik,", price: 18 }];
+    Aufgabe4.treestands = [{ name: "keinen,", price: 0 }, { name: "Holz rustikal,", price: 15 }, { name: "Metall silber,", price: 10 }, { name: "Keramik,", price: 18 }];
     Aufgabe4.delivery = [{ name: "DHL", price: 25 }, { name: "Hermes", price: 20 }, { name: "UPS", price: 40 }];
 })(Aufgabe4 || (Aufgabe4 = {}));
 //# sourceMappingURL=verkauf.js.map
