55703966d4cd61373ec5c9c19852eb8e48693fae
Anja <Anja@Cata-Laptop>
2018-12-05 20:29:38 +0100

    test2

--- Aufgabe_06/Server6.js
+++ Aufgabe_06/Server6.js
@@ -1,5 +1,6 @@
 "use strict";
 const Http = require("http"); //Import eines Modul als HTTP Objekt
+const Url = require("url");
 var L06_SendData;
 (function (L06_SendData) {
     console.log("Starting server"); //Consolelog wird ausgegeben mit nachricht "Starting server"
@@ -16,9 +17,13 @@ var L06_SendData;
     function handleRequest(_request, _response) {
         //console.log("I hear voices")      // gibt Consolelog "I hear voices!" aus
         console.log(_request.url);
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //setzt in den HTML header "content-type" und text/html; charset=utf-8
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Access-Control-Allow-Origin wird auch in den Header gesetzt, damit die antwort des Codes mit dem Nutzer geteilt wird.
-        _response.write(_request.url); //gibt bei _response.write eine angefragte URL aus   
+        let url = Url.parse(_request.url, true).query;
+        console.log(url);
+        for (let key in url) {
+            console.log(url[key]);
+            console.log(key);
+            _response.write(key + " = " + url[key] + "<br>");
+        }
         _response.end(); //beendet die schleife der _response
     }
 })(L06_SendData || (L06_SendData = {}));
--- Aufgabe_06/Server6.ts
+++ Aufgabe_06/Server6.ts
@@ -1,7 +1,12 @@
-
 import * as Http from "http";               //Import eines Modul als HTTP Objekt
+import * as Url from "url";
 
-namespace L06_SendData {                    //
+namespace L06_SendData {        
+         
+      interface Product {
+        [key: string]: number;
+    }
+    
     console.log("Starting server");         //Consolelog wird ausgegeben mit nachricht "Starting server"
     let port: number = process.env.PORT;    //Stellt den PORT als number ein zum Heroku server  
     if (port == undefined)                  //Falls der Port nicht definiert ist,
@@ -20,11 +25,16 @@ namespace L06_SendData {                    //
         //console.log("I hear voices")      // gibt Consolelog "I hear voices!" aus
         console.log(_request.url);
         
-        _response.setHeader("content-type", "text/html; charset=utf-8");        //setzt in den HTML header "content-type" und text/html; charset=utf-8
-        _response.setHeader("Access-Control-Allow-Origin", "*");                //Access-Control-Allow-Origin wird auch in den Header gesetzt, damit die antwort des Codes mit dem Nutzer geteilt wird.
-
-        _response.write(_request.url);          //gibt bei _response.write eine angefragte URL aus   
+        let url: Product = Url.parse(_request.url, true).query;
+        console.log(url);
         
+        for (let key in url) {
+          
+            console.log(url[key]);
+            console.log(key);
+            
+            _response.write(key + " = " + url[key] + "<br>");
+        }
         _response.end();                        //beendet die schleife der _response
     }
 }
