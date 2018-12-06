fe3e4beaac664b6460d24aba8ea37e42f51fe35e
Anja <Anja@Cata-Laptop.eduroam.hs-furtwangen.de>
2018-12-06 11:48:47 +0100

    döldk

--- Aufgabe_06/Server6.js
+++ Aufgabe_06/Server6.js
@@ -1,34 +1,27 @@
 "use strict";
-const Http = require("http"); //Import eines Modul als HTTP Objekt
+const Http = require("http");
 const Url = require("url");
 var L06_SendData;
 (function (L06_SendData) {
-    console.log("Starting server"); //Consolelog wird ausgegeben mit nachricht "Starting server"
-    let port = process.env.PORT; //Stellt den PORT als number ein zum Heroku server  
+    console.log("Starting server");
+    let port = process.env.PORT;
     if (port == undefined)
-        port = 8100; //soll er die nummer 8100 haben (verbindet Heroku mit EIA2)
-    let server = Http.createServer(); //variable server wird als typ Http.Server und wird als Http.creatserver funktion gleichgesetzt
-    server.addListener("request", handleRequest); //F�gt einen Listener hinzu der dem h�ndler bescheid gibt wenn handleRequest getriggert wurde
-    server.addListener("listening", handleListen); //solange der K�ufer auf die funktion zugreift, sieht der H�ndler das.
-    server.listen(port); //schaut zu was f�r ein Port verwendet wird. (8100)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
     function handleListen() {
-        console.log("Listening"); //Consolelog "Listening" wird ausgegeben
+        console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log(_request.url);
-        console.log("request");
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //setzt in den HTML header "content-type" und text/html; charset=utf-8
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Access-Control-Allow-Origin wird auch in den Header gesetzt, damit die antwort des Codes mit dem Nutzer geteilt wird.
-        // _response.write(_request.url);          //gibt bei _response.write eine angefragte URL aus             
-        console.log(_request.url);
-        let url = Url.parse(_request.url, true).query;
-        console.log(url);
-        for (let key in url) {
-            console.log(url[key]);
-            console.log(key);
-            _response.write(key + " = " + url[key] + "<br>");
-        }
-        _response.end(); //beendet die schleife der _response
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        let url = Url.parse(_request.url, true);
+        for (let key in url.query)
+            _response.write(key + ":" + url.query[key] + "<br/>");
+        _response.end();
     }
 })(L06_SendData || (L06_SendData = {}));
 //# sourceMappingURL=Server6.js.map
--- Aufgabe_06/Server6.ts
+++ Aufgabe_06/Server6.ts
@@ -1,47 +1,31 @@
-import * as Http from "http";               //Import eines Modul als HTTP Objekt
+import * as Http from "http";
 import * as Url from "url";
 
-namespace L06_SendData {        
-         
-      interface Product {
-        [key: string]: number;
-    }
-    
-    console.log("Starting server");         //Consolelog wird ausgegeben mit nachricht "Starting server"
-    let port: number = process.env.PORT;    //Stellt den PORT als number ein zum Heroku server  
-    if (port == undefined)                  //Falls der Port nicht definiert ist,
-        port = 8100;                        //soll er die nummer 8100 haben (verbindet Heroku mit EIA2)
+namespace L06_SendData {
+    console.log("Starting server");
+    let port: number = process.env.PORT;
+    if (port == undefined)
+        port = 8100;
 
-    let server: Http.Server = Http.createServer();      //variable server wird als typ Http.Server und wird als Http.creatserver funktion gleichgesetzt
-    server.addListener("request", handleRequest);       //Fügt einen Listener hinzu der dem händler bescheid gibt wenn handleRequest getriggert wurde
-    server.addListener("listening", handleListen);      //solange der Käufer auf die funktion zugreift, sieht der Händler das.
-    server.listen(port);                                //schaut zu was für ein Port verwendet wird. (8100)
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
 
-    function handleListen(): void {                 
-        console.log("Listening");                       //Consolelog "Listening" wird ausgegeben
+    function handleListen(): void {
+        console.log("Listening");
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {  //erstellt eine Funktion die bei einer Http.Incomingmessage, eine Http.ServerResponse zurück schickt
-        console.log(_request.url);
-        console.log("request");
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!");
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");        //setzt in den HTML header "content-type" und text/html; charset=utf-8
-        _response.setHeader("Access-Control-Allow-Origin", "*");                //Access-Control-Allow-Origin wird auch in den Header gesetzt, damit die antwort des Codes mit dem Nutzer geteilt wird.
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
 
-        // _response.write(_request.url);          //gibt bei _response.write eine angefragte URL aus             
-        console.log(_request.url);
-        let url: Product = Url.parse(_request.url, true).query;
-        console.log(url);
-
-
-        for (let key in url) {
-
-            console.log(url[key]);
-            console.log(key);
-
-            _response.write(key + " = " + url[key] + "<br>");
-        }
-
-        _response.end();                        //beendet die schleife der _response
+        let url: Url.Url = Url.parse(_request.url, true);
+        for (let key in url.query)
+            _response.write(key + ":" + url.query[key] + "<br/>");
+     
+        _response.end();
     }
 }
