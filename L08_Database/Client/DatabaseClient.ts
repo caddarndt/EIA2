bda20ba95d459238d773af59d4bb59d3b5d6d3de
Anja <Anja@Cata-Laptop>
2018-12-16 12:14:57 +0100

    herokuapp und mongo server eingesetzt

--- L08_Database/Client/DatabaseClient.ts
+++ L08_Database/Client/DatabaseClient.ts
@@ -1,7 +1,7 @@
 namespace DatabaseClient {
     window.addEventListener("load", init);
-    let serverAddress: string = "http://localhost:8100";
-    //let serverAddress: string = "https://<your>.herokuapp.com/";    
+    //let serverAddress: string = "http://localhost:8100";
+    let serverAddress: string = "https://eia-cata.herokuapp.com/";    
 
     function init(_event: Event): void {
         console.log("Init");
